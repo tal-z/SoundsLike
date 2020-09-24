49966a5af3166d7dcb34f7493f9c2ee64702496d
tal-z <67941052+tal-z@users.noreply.github.com>
2020-09-24 17:31:06 -0400

    Update FindHomophones.py
    updated notes, tested with updated fuzzy_term spellchecker.

--- FindHomophones.py
+++ FindHomophones.py
@@ -2,25 +2,41 @@
 FindHomophones is a module with functions for finding similar-sounding words.
 FindHomophones is the first module of the SoundLike library.
 Created by Tal Zaken using the English Language CMU Dict.
+
 Credits:
 -The CMU Pronouncing Dictionary
 -cmudict python wrapper by David L. Day
 -g2p-en python module by Kyubyong Park
+
 Dependencies:
 -json
 -sys
 -re
 -cmudict
+
 Notes:
+-While this module supports multi-word search terms, it always reduces them to one group of phones.
+ This could lead to some unexpected, but still useful, results. 
+ Resultantly, multi-word results are not supported at this time.
+-Support is not presently offered for multiple pronunciations of a given token.
 -English Language CMU Dict can be swapped out for any other pronunciation dict
  by uncommenting and setting the DictionaryFilepath to point at a JSON file.
--Support is not presently offered for multiple pronunciations of a given token.
+ This would be useful if one wishes to add terms to a custom dictionary.
+
+Ideas:
+-create match pattern for same first and last syllable, and same number of syllables.
+-add multi-word results. Check each word in multi-word search terms,
+ and concatenate all possible results if all words are found.
+ e.g.: "Lee Ann" could return "Leigh Anne," "Lea An," "Lianne," etc.
+-For separate module, figure out "smart selection" results for display.
+
 """
 
 import sys
 import re
 import cmudict
 from GeneratePronunciation import generate_pronunciation
+from FindFuzzyMatch import fuzzy_term
 
 ### Set Pronouncing Dictionary Here ###
 """
